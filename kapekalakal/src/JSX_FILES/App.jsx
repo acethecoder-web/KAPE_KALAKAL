e68b743b0434cdfdaf402d375c90d0d89c6fef60
acehapiz890 <ashapiz@sscgi.com>
2025-07-11 16:06:59 +0800

    2nd commit added lansing page

--- kapekalakal/src/JSX_FILES/App.jsx
+++ kapekalakal/src/JSX_FILES/App.jsx
@@ -1,6 +1,7 @@
 import "../CSS_FILES/App.css";
-import LandingPage from "./LandingPage";
+//import LandingPage from "./LandingPage";
 import NavBar from "./NavBar";
+import About from "./About";
 function App() {
 	return (
 		<>
