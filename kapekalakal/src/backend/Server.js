409a021df55548284f6986becc794d46c06642c9
acehapiz890 <ashapiz@sscgi.com>
2025-07-18 11:06:34 +0800

    14th commit (added the server and api for the creating users)

--- kapekalakal/src/backend/Server.js
+++ kapekalakal/src/backend/Server.js
@@ -1,14 +1,45 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import Accounts from './models/registeracc.model.js';
+
+import {
+    connectDB
+} from './db.js';
 
 dotenv.config();
 
 const app = express();
 
-app.get("/docs", (req, res) => {});
+app.use(express.json());
 
-console.log(process.env.MONGO_URI);
+app.post("/api/docs", async (req, res) => {
+    const account = req.body
+
+    if (!account.name || !account.email || !account.password) {
+        return res.status(400).json({
+            success: false,
+            message: "Please provide all fields"
+        });
+    }
+
+    const newAccount = new Accounts(account);
+
+    try {
+        await newAccount.save();
+        res.status(201).json({
+            success: true,
+            data: newAccount
+        });
+    } catch (error) {
+        console.error("error in Create Product:", error.message);
+        res.status(500).json({
+            success: false,
+            message: "Server Error"
+        });
+    }
+});
 
 app.listen(5174, () => {
+    connectDB();
     console.log("server started at http://localhost:5174");
 });
